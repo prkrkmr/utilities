5b4c5d02bd26f8108262f48b4af56d898010d183
prakhar <prakhar@syonlab.com>
2016-09-03 15:24:58 +0530

    Added the filesorter to git.

---
+++ filesorter.py
@@ -0,0 +1,24 @@
+# Name : Prakhar Kumar
+# Email : prkrkmr@gmail.com
+# File Sorter
+
+
+import os 
+li = []
+li1 = []
+li = os.listdir('./')
+print "Sorting"
+for i in li : 
+    if os.path.isfile(i) == True and i[0] != '.':
+        a = i.split('.')[-1]
+        if os.path.exists(a):
+            if os.path.isfile('./'+a+'/'+i) == True and i.split('.')[0][-1].isdigit() == False:
+                os.rename(i,'./'+a+'/'+i.split('.')[0]+'1.'+i.split('.')[-1])
+            if os.path.isfile('./'+a+'/'+i) == True and i.split('.')[0][-1].isdigit() == True:
+                os.rename(i,'./'+a+'/'+i.split('.')[0].strip(i.split('.')[0][-1])+str(int(i.split('.')[0][-1])+1)+'.'+i.split('.')[-1])
+            else:
+                os.rename(i,'./'+a+'/'+i)
+        else:
+            os.mkdir(a)
+            os.rename(i,'./'+a+'/'+i)
+print "Sorted"
